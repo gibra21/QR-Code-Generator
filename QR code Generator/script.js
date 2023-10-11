191497b1c8da1545a2e48a03948a2268fb8e8348
gibra21 <128503546+gibra21@users.noreply.github.com>
2023-10-11 16:33:51 -0400

    Add files via upload
    Languages used: JavaScript, CSS and HTML

---
+++ QR code Generator/script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = document.querySelector(".form input"),
+generateBtn = document.querySelector(".form button"),
+qrImage = document.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = "Generating QR Code...."
+    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue} `;
+    qrImage.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR code";
+    });
+});
+
+
+qrInput.addEventListener("keyup", () => {
+    if(qrInput.value) {
+        wrapper.classList.remove("active");
+    }
+});
